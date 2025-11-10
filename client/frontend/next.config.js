d77d9bbe78bf2dfc6785525c64bf8b124ab9d330
AmeyaKhanzode <ameyakhanzode2022@gmail.com>
2025-11-10 20:10:26 +0530

    configed backend

--- client/frontend/next.config.js
+++ client/frontend/next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
+    const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://172.28.246.244:8000'
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:8000/:path*',
+        destination: `${backendUrl}/:path*`,
       },
     ]
   },
